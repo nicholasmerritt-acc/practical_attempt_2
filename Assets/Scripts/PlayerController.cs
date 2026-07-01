684a0294ffce0a648f8da2a739be707166a35cae
nicholasmerritt-acc <nicholas.merritt@g.austincc.edu>
2026-06-30 19:47:52 -0500

    jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController cc;
+    private CharacterController characterController;
     private Transform cameraTransform;
+
+    [Header("Jumping")]
+    private bool hasJumped = false;
+    private float gravity = -20f;
+    private float verticalVelocity = 0f;
+    [SerializeField] private float jumpVelocity = 9f;
 
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 5f;
@@ -11,13 +17,39 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        cc = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
         cameraTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //CHECK FALLING and set gravity and velocity
+        bool falling = (verticalVelocity < 0f) && hasJumped;
+        float gravityThisFrame = gravity;
+        if (falling)
+        {
+            gravityThisFrame *= 5f;
+        }
+        verticalVelocity += gravity * Time.deltaTime;
+
+        //CHECK GROUNDED
+        if (characterController.isGrounded)
+        {
+            if (verticalVelocity < 0f)
+            {
+                verticalVelocity = -2f;
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpVelocity;
+                Debug.Log("jump");
+                hasJumped = true;
+            }
+        }
+        Vector3 upVelocity = verticalVelocity * Vector3.up;
+
+
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
 
@@ -33,7 +65,6 @@ public class PlayerController : MonoBehaviour
         Vector3 crmd = (cameraRight * moveX + cameraForward * moveZ).normalized;
 
         //calc camera directed move direction
-        //Quaternion.LookRotation(cameraForward, cameraRight, )
         if (crmd.sqrMagnitude > .001f)
         {
             //LOOK ROTATION and SLERP
@@ -41,10 +72,7 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
 
-
-        //Vector3 moveDirection = new(moveX, 0f, moveZ);
         Vector3 finalMove = crmd * moveSpeed;
-
-        cc.Move(finalMove * Time.deltaTime);
+        characterController.Move((finalMove + upVelocity) * Time.deltaTime);
     }
 }
